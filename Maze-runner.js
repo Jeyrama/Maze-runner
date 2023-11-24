2794e44b9dee18725ab55adbf40f5d3061d4d3b5
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-24 13:51:21 -0800

    Update Maze-runner.js

--- Maze-runner.js
+++ Maze-runner.js
@@ -42,3 +42,27 @@ Rules:
 
 
 // Solution
+
+function mazeRunner(maze, directions) {
+  let path = Array(), curX, curY;
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[i].length; j++) {
+      if (maze[i][j] == 2) {
+        curX = j;
+        curY = i;
+      }
+    }
+  }
+  for (let i = 0; i < directions.length; i++) {
+    if (directions[i] == 'N') curY--;
+    if (directions[i] == 'S') curY++;
+    if (directions[i] == 'W') curX--;
+    if (directions[i] == 'E') curX++;
+    
+    // path.push(maze[curY][curX]);
+    if (!maze[curY]) return 'Dead';
+    if (maze[curY][curX] == 3) return 'Finish';
+    if (maze[curY][curX] == undefined || maze[curY][curX] == 1) return 'Dead';
+  }
+  return 'Lost';
+}
